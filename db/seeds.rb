c7627d2d963759e2793f323c8b7452249ca02c11
Diego <diego.tanel@tecneg.com>
2012-12-19 18:19:21 -0300

    Se actualizo seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+#encoding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,10 +7,55 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-@user = User.create!(:name => "admin", :email => "admin@admin.com",
+@admin = User.create!(:name => "admin", :email => "admin@admin.com",
+                     :password => "123456", :password_confirmation => "123456")
+@admin.toggle!(:admin)
+
+@user = User.create!(:name => "ocarballa", :email => "ocarballa@taesa.com.ar",
                      :password => "123456", :password_confirmation => "123456")
 @user.toggle!(:admin)
 
-valid_attributes = {:user_id => @user, :valor => 1}
+User.create!(:name => "arobles", :email => "arobles@taesa.com.ar",
+                     :password => "123456", :password_confirmation => "123456")
+
+Banco.create!(:detalle => "Banco Provincia de Buenos Aires")
+Banco.create!(:detalle => "Citibank NA")
+
+Empresa.create!(:detalle => "Ariste de Estrugamou SAAGI")
+Empresa.create!(:detalle => "Lobiña SA")
+Empresa.create!(:detalle => "Calmin SA")
+Empresa.create!(:detalle => "TAESA AGEISA")
+Empresa.create!(:detalle => "Dalmon SA")
+Empresa.create!(:detalle => "Chantaco SA")
+
+Solicitante.create!(:detalle => "Fernando Buisan")
+Solicitante.create!(:detalle => "Osvaldo Carballa")
+
+CanalDeSolicitud.create!(:detalle => "Telefónico")
+CanalDeSolicitud.create!(:detalle => "Mail")
+
+Rubro.create!(:detalle => "Trabajos rurales")
+Rubro.create!(:detalle => "Agroquímicos y fertilizantes")
+Rubro.create!(:detalle => "Impuestos")
+Rubro.create!(:detalle => "Hacienda")
+
+ClienteProveedor.create!(:detalle => "Jukic")
+ClienteProveedor.create!(:detalle => "ASP")
+ClienteProveedor.create!(:detalle => "Ferrari")
+
+ProductoTrabajo.create!(:detalle => "Cosecha")
+ProductoTrabajo.create!(:detalle => "Combustible")
+ProductoTrabajo.create!(:detalle => "Impuesto a las ganancias")
+
+MedioDePago.create!(:detalle => "Cheque")
+MedioDePago.create!(:detalle => "Cheque diferido")
+MedioDePago.create!(:detalle => "Transferencia eléctronica")
+MedioDePago.create!(:detalle => "Efectivo")
+
+MotivoDeBajaPresupuestaria.create!(:detalle => "Cumplida")
+MotivoDeBajaPresupuestaria.create!(:detalle => "No se realizará la compra o gasto")
+
+
+valid_attributes = {:user_id => @user, :valor => 1, :valor => Money.new(4, "ARS")}
 @cotizacion_peso_dolar = CotizacionPesoDolar.create! valid_attributes
 @saldo_bancario = SaldoBancario.create! valid_attributes
