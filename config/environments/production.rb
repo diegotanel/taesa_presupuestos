c4f63371313ccfc34526cf8b095ef847791ebe4e
Diego <diego.tanel@tecneg.com>
2013-03-27 10:05:13 -0300

    Cambio de configuracion para produccion, se compilan los asset y se agrego custom_print.css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ SampleApp::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -44,7 +44,7 @@ SampleApp::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( custom.css blueprint/screen.css blueprint/print.css blueprint/ie.css)
+  config.assets.precompile += %w( custom.css custom_print.css blueprint/screen.css blueprint/print.css blueprint/ie.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
